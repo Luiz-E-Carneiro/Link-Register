19f0da7a4d046c2af2dc9ab149acfc45d43bfa03
Luiz-E-Carneiro <luizecarneiro2006@gmail.com>
2024-05-05 22:36:25 -0300

    Remove heart action

--- backend/heart.php
+++ backend/heart.php
@@ -6,6 +6,6 @@ if(!isset($_POST['linkId'])){
     header('Location: ./../frontend/index.php?erro=3');
 } else {
     $id = $_POST['linkId'];
-    $_SESSION['links'][$id]['heart'] = true;
+    $_SESSION['links'][$id]['heart'] === true ? $_SESSION['links'][$id]['heart'] = false : $_SESSION['links'][$id]['heart'] = true;
     header('Location: ./../frontend/index.php');
 }
